7d0825f760308dade10dadcb6c3fcfa74e3047c6
khalidsy90 <khalidhamedi3@gmail.com>
2021-07-25 16:22:12 +0300

    edit local storage

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
+books=[]
+newbooks=[]
 function BookShoop(bookname,bookprice){
     this.bookname=bookname
     this.bookprice=bookprice
-    let bookPages=function() {
-        min = Math.ceil(1);
-        max = Math.floor(500);
-        return Math.floor(Math.random() * (max - min) + min); 
-      }
+    books.push(this)
    
 }
+
 BookShoop.prototype.bookPages=function() {
     min = Math.ceil(1);
     max = Math.floor(500);
     return Math.floor(Math.random() * (max - min) + min); 
   }
+
+  let bookPage=function() {
+    min = Math.ceil(1);
+    max = Math.floor(500);
+    return Math.floor(Math.random() * (max - min) + min); 
+  }
+  
 
 let table = document.createElement("table");
 let conatiner=document.querySelector('div[class=container]')
@@ -22,11 +28,14 @@ let resultsction=document.querySelector('section[class=resultsction]')
 let form=document.querySelector('form[id=deatils]')
 let btnSub=document.querySelector('input[type=submit]')
 let tot=document.createElement('h3')
+let grandtotal=0
 
 
 
 
-btnSub.addEventListener('click',function(){
+btnSub.addEventListener('click',function(){   
+    console.log(books);
+    savetolocal()
     tNewRow()
     grandtotal=0
     total()
@@ -52,12 +61,17 @@ function tHeader(){
     table.appendChild(tr);
 }
 tHeader()
-
+displyData()
+if(table.rows.length > 0){
+    grandtotal=0
+    total()
+    tot.remove()
+    tot.textContent='Total :  '+grandtotal
+    resultsction.appendChild(tot)
+}
 function tNewRow(){
-
     let bookName=document.getElementById('bookname').value
     let bookPrice=document.getElementById('bookprice').value    
-
     if(bookName == '' || bookPrice == ''){
         alert('Please Enter Book deatils')
         return
@@ -66,25 +80,24 @@ function tNewRow(){
         alert("please Enter Just Numbers")
         return
     }
-    let book =new BookShoop(bookName,bookPrice)
 
-    let tr=document.createElement("tr");
-    let tdName=document.createElement("td");
-    tdName.textContent=book.bookname
+   let book =new BookShoop(bookName,bookPrice)
+            let tr=document.createElement("tr");
+            let tdName=document.createElement("td");
+            tdName.textContent=book.bookname
+            let tdPages=document.createElement("td");
+            tdPages.textContent=book.bookPages()
 
-    let tdPages=document.createElement("td");
-    tdPages.textContent=book.bookPages()
+            let tdPrice=document.createElement("td");
+            tdPrice.textContent=book.bookprice
 
-    let tdPrice=document.createElement("td");
-    tdPrice.textContent=book.bookprice
-    
-    tr.appendChild(tdName)
-    tr.appendChild(tdPages)
-    tr.appendChild(tdPrice)
-
-    table.appendChild(tr)
+            tr.appendChild(tdName)
+            tr.appendChild(tdPages)
+            tr.appendChild(tdPrice)
+            table.appendChild(tr)
 }
-let grandtotal=0
+
+
 function total(){
     for (let i = 0; i < table.rows.length; i++) {
         if(i==0){
@@ -94,4 +107,37 @@ function total(){
        grandtotal+=parseInt(row[2].textContent)
     }
 }
+
+function savetolocal(){
+    localStorage.setItem('books',JSON.stringify(books))
+}
+
+function readfromlocalstor(){
+    let data=[]
+    data=JSON.parse(localStorage.getItem('books'))
+    newbooks=data
+    books=newbooks
+    return data
+}
+
+function displyData(){
+    if(localStorage.length>0){
+             for (let i = 0; i < readfromlocalstor().length; i++) { 
+             let tr=document.createElement("tr");
+             let tdName=document.createElement("td");
+             tdName.textContent=readfromlocalstor()[i].bookname
+             let tdPages=document.createElement("td");
+             tdPages.textContent=bookPage()
+ 
+             let tdPrice=document.createElement("td");
+             tdPrice.textContent=readfromlocalstor()[i].bookprice
+ 
+             tr.appendChild(tdName)
+             tr.appendChild(tdPages)
+             tr.appendChild(tdPrice)
+             table.appendChild(tr)
+         }
+    }
+}
+
 
